a957e37d1811f84e7cc07e2a9ab87a6f71a9c86f
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2025-02-16 11:55:41 +0530

    Add mongoose database connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 5432;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
