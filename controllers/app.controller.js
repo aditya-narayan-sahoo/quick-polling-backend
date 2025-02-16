82fee0753129b026b328791ae12d3183e4a4cba7
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2025-02-16 12:15:30 +0530

    Add get Poll and delete poll feature

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -43,3 +43,29 @@ export const getAllPolls = async (req, res) => {
     res.status(500).json({ error: "Failed to fetch polls" });
   }
 };
+
+export const getPoll = async (req, res) => {
+  try {
+    const poll = await Poll.findById(req.params.id);
+    if (!poll) {
+      return res.status(404).json({ error: "Poll not found" });
+    }
+    res.status(200).json(poll);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch poll" });
+  }
+};
+
+export const deletePoll = async (req, res) => {
+  try {
+    const poll = await Poll.findByIdAndDelete(req.params.id);
+    if (!poll) {
+      return res.status(404).json({ error: "Poll not found" });
+    }
+    res.status(200).json({ message: "Poll deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to delete poll" });
+  }
+};
--- routes/app.routes.js
+++ routes/app.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import {
   createPoll,
+  deletePoll,
   getAllPolls,
+  getPoll,
   votePoll,
 } from "../controllers/app.controller.js";
 
@@ -9,6 +11,8 @@ const router = Router();
 
 router.post("/polls", createPoll);
 router.post("/polls/:id/vote", votePoll);
+router.get("/polls/:id", getPoll);
 router.get("/polls", getAllPolls);
+router.delete("/polls/:id", deletePoll);
 
 export default router;
