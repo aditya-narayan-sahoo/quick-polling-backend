82d3d6f920b134887eac8a1868c61a2892678b11
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2025-02-16 11:54:05 +0530

    Initialize backend using express

---
+++ index.js
@@ -0,0 +1,15 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5432;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
